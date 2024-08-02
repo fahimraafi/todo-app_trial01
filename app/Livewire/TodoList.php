3bbdff8dfad5de72c92c3657fcd64ded0676d412
fahimraafi <fahim.raafi.dev@gmail.com>
2024-08-03 02:51:38 +0600

    Task created, list shown with pagination

--- app/Livewire/TodoList.php
+++ app/Livewire/TodoList.php
@@ -2,12 +2,33 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Validate;
 use Livewire\Component;
+use App\Models\Todo;
+use Livewire\WithPagination;
 
 class TodoList extends Component
 {
+    use WithPagination;
+
+    #[Validate('required|min:3')]
+    public $name;
+
+    public $search;
+
+
+    public function create(){
+
+        $validated = $this->validateOnly('name');
+
+        Todo::create($validated);
+        session()->flash('success','Successfully created');
+
+    }
     public function render()
     {
-        return view('livewire.todo-list');
+        return view('livewire.todo-list', [
+            "todos" => Todo::latest()->paginate(5),
+        ]);
     }
 }
--- resources/views/livewire/todo-list.blade.php
+++ resources/views/livewire/todo-list.blade.php
@@ -1,3 +1,30 @@
 <div>
-    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
+    <div id="content" class="mx-auto" style="max-width:500px;">
+        <div class="container content py-6 mx-auto">
+            <div class="mx-auto">
+
+                @include('livewire.includes.create-todo-box')
+
+            </div>
+        </div>
+        <div id="search-box" class="flex flex-col items-center px-2 my-4 justify-center">
+            <div class="flex justify-center items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                        d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                </svg>
+                <input type="text" placeholder="Search..."
+                    class="bg-gray-100 ml-2 rounded px-4 py-2 hover:bg-gray-100" />
+            </div>
+            <span class="text-red-500 text-xs block mt-2">Error</span>
+
+        </div>
+
+        @foreach ($todos as $todo)
+            @include('livewire.includes.todo-card')
+        @endforeach
+
+
+    </div>
 </div>
